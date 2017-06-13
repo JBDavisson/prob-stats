4dcda839cd4e3d2002423c08efc90dbf7cbe4a5b
Jacob Davisson <davissonjb@gmail.com>
2017-06-13 08:27:52 -0400

    Added binomial and poisson distributions to file: prob.h

--- prob.h
+++ prob.h
@@ -2,17 +2,31 @@
 #define __PROB__H__
 
 #include "utils.h"
-
 #include <cmath>
 #include <cstdio>
 
-
 template <typename T = int, typename S = double>
-S binomialDist(T n, T k, S c)
+S binomDist(T n, T k, S p)
 {
+	S q = (1 - p);
 	T bin = binomCoeff(n, k);
-	return T();
-
+	S temp = S();
+	if(bin == 0 || q <= 0) 
+	{ 
+		temp = 0; 
+	}
+	else
+	{
+		S success = std::pow(p, k);
+		S failure = std::pow(q, (n-k));
+		temp = bin*success*failure;
+		/*printf("q: %f\n", q);
+		printf("bin: %d\n", bin);
+		printf("success: %f\n", success);
+		printf("failure: %f\n", failure);
+		printf("temp: %f\n", temp);*/
+	}
+	return temp;
 }
 
 template <typename T = int, typename S = double>
@@ -26,6 +40,6 @@ S poissonDist(S lambda, T x)
 	//printf("a = %f\n", a);
 	//printf("b = %f\n", b);
 	return (S)p;
-
+}
 
 #endif
