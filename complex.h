c210d72818204ff360595fc7c52d0387f9553fb1
JBDavisson <jbd@ku.edu>
2017-07-03 23:24:46 -0500

    Modified program file structure to reflect future changes.

---
+++ complex.h
@@ -0,0 +1,50 @@
+#ifndef __COMPLEX__HH__
+#define __COMPLEX__HH__
+
+#include <cmath>
+#include <iostream>
+
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825
+
+typedef double_t Component;
+typedef double_t Magnitude;
+typedef double_t Phase;
+
+template <typename T>
+class Complex
+{
+	private:
+		Component 	a;
+		Component 	b;
+		Magnitude 	mag;
+		Phase 		ang;
+		void 		computeMagnitude();
+		void 		computePhase();
+	
+	public:
+		//Constructor methods.
+		Complex<T>();
+		Complex<T>(const Component a, const Component b);
+		
+		//Begin non-constructor methods.
+		Component		getRealPart() const;
+		Component		getImaginaryPart() const;
+		Magnitude 		getMagnitude() const;
+		Phase 			getPhase(bool rad) const;
+		void			setComponents(const Component a, const Component b);
+		void	 		complexConjugate();
+		//Operator Overloads
+		Complex<T> operator + (Complex<T> com);
+		Complex<T> operator * (Complex<T> com);
+		friend std::ostream& operator << (std::ostream& os, Complex<T> com)
+		{
+			os << "(" << com.getRealPart() << ", j" << com.getImaginaryPart() << ")";
+			return os;
+		}
+		
+		//Class object destructor
+		~Complex();
+};
+
+#include "complex.hpp"
+#endif
--- mathTypes.h
+++ mathTypes.h
@@ -1,50 +1,6 @@
 #ifndef __MATH__TYPES__HH__
 #define __MATH__TYPES__HH__
 
-#include <cmath>
-#include <iostream>
+#include "complex.h"
 
-#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825
-
-typedef double_t Component;
-typedef double_t Magnitude;
-typedef double_t Phase;
-
-template <typename T>
-class Complex
-{
-	private:
-		Component 	a;
-		Component 	b;
-		Magnitude 	mag;
-		Phase 		ang;
-		void 		computeMagnitude();
-		void 		computePhase();
-	
-	public:
-		//Constructor methods.
-		Complex<T>();
-		Complex<T>(const Component a, const Component b);
-		
-		//Begin non-constructor methods.
-		Component		getRealPart() const;
-		Component		getImaginaryPart() const;
-		Magnitude 		getMagnitude() const;
-		Phase 			getPhase(bool rad) const;
-		void			setComponents(const Component a, const Component b);
-		void	 		complexConjugate();
-		//Operator Overloads
-		Complex<T> operator + (Complex<T> com);
-		Complex<T> operator * (Complex<T> com);
-		friend std::ostream& operator << (std::ostream& os, Complex<T> com)
-		{
-			os << "(" << com.getRealPart() << ", j" << com.getImaginaryPart() << ")";
-			return os;
-		}
-		
-		//Class object destructor
-		~Complex();
-};
-
-#include "mathTypes.hpp"
 #endif
